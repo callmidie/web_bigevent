f41a07fe5f7f36f5701e4b9f0579eb78e1db7330
acer <164291723@qq.com>
2021-06-23 23:32:37 +0800

    完成个人中心功能的开发

--- assets/js/index.js
+++ assets/js/index.js
@@ -30,7 +30,7 @@ function getUserInfo() {
         // },
         success: function(res) {
             if (res.status !== 0) {
-                return layui.layer.msg('获取用户信息失败！')
+                return layer.msg('获取用户信息失败！')
             }
 
             // 调用 renderAvatar 渲染用户基本信息
